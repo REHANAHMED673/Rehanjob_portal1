916e0eeba150eb550dc09508015c11676f2da932
Your Name <yourgmail@example.com>
2026-05-21 00:33:44 +0530

    fixed allowed hosts

--- rehan_job_portal/rehan_job_portal/settings.py
+++ rehan_job_portal/rehan_job_portal/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # =========================
 SECRET_KEY = 'django-insecure-change-this-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rehanjob-portal1.onrender.com', '127.0.0.1', 'localhost']
 
 
 # =========================
